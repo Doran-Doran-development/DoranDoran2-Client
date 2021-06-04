01dad588f770b81ab0e9a688dacecaea5ec7c039
towel1017 <48384442+towel1017@users.noreply.github.com>
2021-06-04 13:57:35 +0900

    [Add] Recoil 사용을 위한 RecoilRoot 태그로 감싸기

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, BrowserRouter as Router } from "react-router-dom";
 import { AdminEscapePage, HomePage, LoginPage, MainOutingPage, MainPage, MainRoomPage, Mypage, OutingPage, RentalRoomPage, RoomDetailPage, SignupPage } from "pages";
+import { RecoilRoot } from "recoil";
 
 const App: React.FC = () => {
   return (
     <>
-      <Route exact path="/" component={MainPage} />
-      <Route exact path="/" component={MainRoomPage} />
-      <Route exact path="/" component={MainOutingPage} />
-      <Route path="/mypage" component={Mypage} />
-      <Route path="/home" component={HomePage} />
-      <Route path="/outing" component={OutingPage} />
-      <Route path="/room/:roomdetail" component={RoomDetailPage} />
-      <Route path="/rental/:roomname" component={RentalRoomPage} />
-      <Route path="/login" component={LoginPage} />
-      <Route path="/signup" component={SignupPage} />
-      <Route path="/escape" component={AdminEscapePage} />
+      <RecoilRoot>
+        <Router>
+          <Route exact path="/" component={MainPage} />
+          <Route exact path="/" component={MainRoomPage} />
+          <Route exact path="/" component={MainOutingPage} />
+          <Route path="/mypage" component={Mypage} />
+          <Route path="/home" component={HomePage} />
+          <Route path="/outing" component={OutingPage} />
+          <Route path="/room/:roomdetail" component={RoomDetailPage} />
+          <Route path="/rental/:roomname" component={RentalRoomPage} />
+          <Route path="/login" component={LoginPage} />
+          <Route path="/signup" component={SignupPage} />
+          <Route path="/escape" component={AdminEscapePage} />
+        </Router>
+      </RecoilRoot>
     </>
   );
 };
